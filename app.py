b61d6cca5f4b4c62e67576747fe8dc90eb493700
Msahana36 <sahana_m@pursuitsoftware.biz>
2024-06-26 16:52:38 +0530

    Added Hastings Insurance Documents

--- app.py
+++ app.py
@@ -1,26 +1,39 @@
 import os
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+import json
+from llama_index.core import SimpleDirectoryReader, VectorStoreIndex, StorageContext, load_index_from_storage
 from llama_index.core import Settings
 from llama_index.llms.gemini import Gemini
 from llama_index.embeddings.gemini import GeminiEmbedding
-import json
-from flask import Flask,  request
+from llama_index.core.memory import ChatMemoryBuffer
+from flask import Flask, request
 from flask_cors import CORS, cross_origin
 
 
-api_key=os.getenv("GEMINI_API_KEY") 
-
-data = SimpleDirectoryReader(input_dir="data").load_data()
+api_key = os.getenv("GEMINI_API_KEY")
+index_storage_dir = "index_storage"
+data_dir = "data"
 
 Settings.embed_model = GeminiEmbedding(
     model_name="models/embedding-001", api_key=api_key
 )
 
+Settings.llm = Gemini(api_key=api_key, temperature=0)
 
-Settings.llm = Gemini(api_key=api_key,temperature=0)
-index = VectorStoreIndex.from_documents(data)
 
-from llama_index.core.memory import ChatMemoryBuffer
+if not os.path.exists(index_storage_dir):
+    print("Creating new index...")
+    # load the documents and create the index
+    documents = SimpleDirectoryReader(input_dir=data_dir).load_data()
+    index = VectorStoreIndex.from_documents(documents)
+    # store it for later
+    index.storage_context.persist(persist_dir=index_storage_dir)
+else:
+    # load the existing index
+    print("Loading existing index...")
+    storage_context = StorageContext.from_defaults(persist_dir=index_storage_dir)
+    index = load_index_from_storage(storage_context)
+    
+print("Index loaded successfully.")
 
 memory = ChatMemoryBuffer.from_defaults(token_limit=4000)
 
@@ -29,21 +42,17 @@ chat_engine = index.as_chat_engine(
     memory=memory,
     system_prompt=(
         """You are a friendly chatbot, able to have normal interactions.
-        You help the users with their questions. Return answers from the document only. 
-        Do not make up your own answers.
-        
-        """
-        
+        You help the users with their questions. Return answers from the stored documents only.
+        Summarize the answers in less than or up to three sentences.
+        Do not make up your own answers. """
     ),
 )
 
-
-
 app = Flask(__name__)
-cors = CORS(app=app) 
-app.config['CORS_HEADERS'] = 'Content-Type' 
-
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 app.static_folder = 'static'
+
 @app.route("/")
 def home():
     return "KB Search API "
